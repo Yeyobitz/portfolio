6f27fd800bb2956a5cf44b22ac7eb7a3f0a49b38
Panshulo <panshulo@panshulo.dev>
2026-06-17 02:35:20 -0400

    fix: translate project highlights to Spanish, remove broken asset links

--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_Dfi0OO7k.mjs';
+import { manifest } from './manifest_B40Nk8HB.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
