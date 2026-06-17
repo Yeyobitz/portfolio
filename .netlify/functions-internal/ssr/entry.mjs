9bd2c386bb944235718e629cb2378623e078321a
Panshulo <panshulo@panshulo.dev>
2026-06-17 02:04:24 -0400

    fix: page title and skills grouping by category field

--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_CdRYMVCh.mjs';
+import { manifest } from './manifest_Dfi0OO7k.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
